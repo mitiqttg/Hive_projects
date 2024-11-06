737c092470fffe9d433c292c11665da468137e12
mitifromqt <minhtien2812000@gmail.com>
2024-11-06 04:45:22 +0200

    full dir updated

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,3 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void    *ft_memmove(void *s1, const void *s2, size_t n);
+{
+	size_t							i;
+	const unsigned char				*dest;
+	unsigned char					value;
+
+	i = 0;
+	dest = (const unsigned char *)s;
+	value = (unsigned char)c;
+	while ((i < n + 1) && (dest[i] != '\0') && (value[i] != '\0'))
+	{
+		if (dest[i] == value)
+			return ((void *)(dest + i));
+		i++;
+	}
+	return (NULL);
+}
